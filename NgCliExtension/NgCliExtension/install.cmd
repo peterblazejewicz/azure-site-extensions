ba5dcf6f7539111fbf1d2cec0cbbfdc1ff8eef0b
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-10-28 22:59:29 +0200

    Bump angular-cli version in extension

--- NgCliExtension/NgCliExtension/install.cmd
+++ NgCliExtension/NgCliExtension/install.cmd
@@ -2,4 +2,4 @@ echo off
 :: install specific verison of angular-cli (NG) tool
 npm uninstall --global angular-cli
 npm cache clear
-npm install --global angular-cli@1.0.0-beta.18
+npm install --global angular-cli@1.0.0-beta.19
